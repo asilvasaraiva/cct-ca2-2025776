713d39bcae7715331bcc8f5b2a0a2aa5b3e8c4d1
Alexsandro saraiva <alex_saraiva14@hotmail.com>
2026-05-05 10:07:32 +0100

    Creating the project

---
+++ CA2/src/main/java/com/cct/ca2/CA2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cct.ca2;
+
+/**
+ *
+ * @author alexsandrosaraiva
+ */
+public class CA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
