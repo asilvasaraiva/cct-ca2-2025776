6f332eda6918e0a49915f6d5f5a0a453d6303136
Alexsandro saraiva <alex_saraiva14@hotmail.com>
2026-05-10 23:40:41 +0100

    Creating packages Hierarchy

---
+++ CA2/src/main/java/com/cct/ca_2/validator/InputValidator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cct.ca_2.validator;
+
+/**
+ *
+ * @author alexsandrosaraiva
+ */
+public class InputValidator {
+    
+}
