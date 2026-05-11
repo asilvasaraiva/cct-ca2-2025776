6eb6b430ec476cc6486d17875030873a2c95545c
Alexsandro saraiva <alex_saraiva14@hotmail.com>
2026-05-11 18:35:39 +0100

    Creating Utility Classes SearchUtils and SortUtils
    Utility classes that handles the functions for search logic and sort(printing on the screen)

--- CA2/src/main/java/com/cct/ca_2/utils/SearchUtil.java
+++ CA2/src/main/java/com/cct/ca_2/utils/SearchUtil.java
@@ -4,10 +4,38 @@
  */
 package com.cct.ca_2.utils;
 
+import com.cct.ca_2.datastructures.BinarySearch;
+import com.cct.ca_2.datastructures.MergeSort;
+import com.cct.ca_2.entities.Employee;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author alexsandrosaraiva
  */
 public class SearchUtil {
-    
+     public static List<Employee> searchAndDisplay(List<Employee> employees, Scanner scanner) {
+        List<Employee> sorted = MergeSort.sort(employees);
+
+        System.out.println("\n SEARCH selected - Binary Search (Recursive)\n");
+
+        String firstName = InputValidator.getValidName(scanner, "Enter first name: ");
+        String lastName  = InputValidator.getValidName(scanner, "Enter last name: ");
+        String target = firstName + " " + lastName;
+
+        int index = BinarySearch.search(sorted, target, 0, sorted.size() - 1);
+
+        if (index != -1) {
+            Employee found = sorted.get(index);
+            System.out.println("\n*** RECORD FOUND ***");
+            System.out.println("Name: " + found.getFullName());
+            System.out.println("Manager Type: " + found.getManagerType());
+            System.out.println("Department: " + found.getDepartment());
+        } else {
+            System.out.println("\n\"" + target + "\" was NOT found in the list.");
+        }
+
+        return sorted;
+    }
 }
--- CA2/src/main/java/com/cct/ca_2/utils/SortUtils.java
+++ CA2/src/main/java/com/cct/ca_2/utils/SortUtils.java
@@ -4,10 +4,26 @@
  */
 package com.cct.ca_2.utils;
 
+import com.cct.ca_2.datastructures.MergeSort;
+import com.cct.ca_2.entities.Employee;
+import java.util.List;
+
 /**
  *
  * @author alexsandrosaraiva
  */
 public class SortUtils {
+       public static List<Employee> sortAndDisplay(List<Employee> employees) {
+        List<Employee> sorted = MergeSort.sort(employees);
+
+        System.out.println("\nSORT selected - Merge Sort (Recursive)\n");
+
+        for (int i = 0; i < sorted.size(); i++) {
+            System.out.println((i + 1) + ". " + sorted.get(i).getFullName());
+        }
+        System.out.println("\nTotal records: " + sorted.size());
+
+        return sorted;
+    }
     
 }
